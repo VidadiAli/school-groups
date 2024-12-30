5c445e8ff61f9929601eb2e2dce051fb9cf24ac6
VidadiAli <vidadiali.tiva@gmail.com>
2024-12-30 04:38:21 +0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, Router } from 'react-router-dom';
 import Girish from './Components/Girish/Girish';
 import User from './Components/User/User';
 
 const App = () => {
   return (
-    <BrowserRouter basename="/school-groups"> {/* Basename əlavə edildi */}
+    <Router basename="/school-groups"> {/* Basename əlavə edildi */}
       <Routes>
         <Route path="/" element={<Girish />} />
         <Route path="/:user" element={<User />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
